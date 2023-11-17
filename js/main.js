5af3d75773169637e57dc8d0fa7bb593b7bc89e5
tbelton1160 <travithebest@gmail.com>
2023-11-17 05:25:44 -0500

    not sure this went through

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 //The the constructor needs to have a query selector so each player can select a box
 //I need to create a function to check for wins, it needs if/else statements for winning conditions
 //the function needs a way to pull the value from each box in order to check for wins
-//each player needs a method that can differentiate whos turn it is, after the method is called it should change it to the other players turn
+//each player needs a method that can differentiate whos turn it is, after the method is called it should change it to the other players turnn
 
 class TicTacToe{
 	constructor(targetBox){//targetbox is the paramater that we need for each child object and differentiates all of them
